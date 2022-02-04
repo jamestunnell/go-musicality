50cda4af0f2478d26c9b3d6974a2fbc137b0724a
James Tunnell <james_tunnell@selinc.com>
2022-02-04 10:26:58 -0800

    Make up default channel assignments if MIDI settings are missing

--- performance/midi/settings.go
+++ performance/midi/settings.go
@@ -2,7 +2,6 @@ package midi
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/jamestunnell/go-musicality/notation/score"
@@ -12,24 +11,40 @@ type MIDISettings struct {
 	PartChannels map[string]uint8 `json:"partChannels"`
 }
 
-var errMIDISettingsNotFound = errors.New("MIDI settings not found")
-
 func Settings(s *score.Score) (*MIDISettings, error) {
-	obj, found := s.Settings["midi"]
-	if !found {
-		return nil, errMIDISettingsNotFound
-	}
-
-	d, err := json.Marshal(obj)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal MIDI settings obj: %w", err)
-	}
-
 	var midiSettings MIDISettings
 
-	err = json.Unmarshal(d, &midiSettings)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal as MIDI settings: %w", err)
+	obj, found := s.Settings["midi"]
+	if found {
+		d, err := json.Marshal(obj)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal MIDI settings obj: %w", err)
+		}
+
+		err = json.Unmarshal(d, &midiSettings)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal as MIDI settings: %w", err)
+		}
+	} else {
+		partChannels := map[string]uint8{}
+		channel := uint8(1)
+
+		for _, partName := range s.PartNames() {
+			partChannels[partName] = channel
+
+			switch channel {
+			case 9: // reserve channel 10 for percussion
+				channel = 11
+			case 16: // wrap around once last channel is reached
+				channel = 1
+			default:
+				channel++
+			}
+		}
+
+		midiSettings = MIDISettings{
+			PartChannels: partChannels,
+		}
 	}
 
 	return &midiSettings, nil
